d6d07f6085d61794cb5f55269ebb35a1ebb29ab6
ipewzner <48483845+ipewzner@users.noreply.github.com>
2019-12-15 16:07:01 +0200

    001

---
+++ BE/GuestRequest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class GuestRequest
+    {
+        int GuestRequestKey;
+        string PrivateName;
+        string FamilyName;
+        string MailAddress;
+        bool Status;
+        DateTime RegistrationDate;
+        DateTime EntryDate;
+        DateTime ReleaseDate;
+        int Area;
+        int SubArea;
+        int Type;
+        int Adults;
+        int Children;
+        bool Pool;
+        bool Jacuzzi;
+        bool Garden;
+        bool ChildrensAttractions;
+
+        //----------------function-------------------
+        GuestRequest()
+        {
+            if ((GuestRequestKey > 99999999) || (GuestRequestKey < 0))
+                throw new IndexOutOfRangeException();
+
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
