4e1e4d61e5800f19fa7c0f78d7546857f3ccf263
ipewzner <48483845+ipewzner@users.noreply.github.com>
2020-02-07 12:11:09 +0200

    2.3

--- PLWPF/Windows/Statistics/GuestRequestQueryPage.xaml.cs
+++ PLWPF/Windows/Statistics/GuestRequestQueryPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Windows.Controls;
 using System.ComponentModel;
 using System.Windows.Data;
+using System.Windows;
 
 namespace PLWPF.Windows.Statistics
 {
@@ -13,38 +14,83 @@ namespace PLWPF.Windows.Statistics
     {
         MyBl myBL = new MyBl();
         CollectionView view;
+
         public GuestRequestQueryPage()
         {
             InitializeComponent();
-
-            //**********************************************************
-
-            GuestRequestListView.ItemsSource = myBL.GuestRequestOrderBy_Location().SelectMany(x=>x);
-             view = (CollectionView)CollectionViewSource.GetDefaultView(GuestRequestListView.ItemsSource);
-            PropertyGroupDescription groupDescription = new PropertyGroupDescription("Area");
-            view.GroupDescriptions.Add(groupDescription);
-
+            GuestRequestListView.ItemsSource = myBL.GuestRequestBy();
+            view = (CollectionView)CollectionViewSource.GetDefaultView(GuestRequestListView.ItemsSource);
         }
 
         private void GuestRequestListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            view.SortDescriptions.Add(new SortDescription("EntryDate", ListSortDirection.Ascending));
         }
+        
+        //-----------------------------------------------------------------------------------------------
+
+        GridViewColumnHeader _lastHeaderClicked = null;
+
+        private void ListViewColumnHeaderClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GridViewColumnHeader headerClicked = e.OriginalSource as GridViewColumnHeader;
+
+            if (headerClicked == null)
+                return;
+
+            if (headerClicked.Role == GridViewColumnHeaderRole.Padding)
+                return;
+
+            var sortingColumn = (headerClicked.Column.DisplayMemberBinding as Binding)?.Path?.Path;
+            if (sortingColumn == null)
+                return;
+
+            var direction = ApplySort(view, sortingColumn);
+                  
+
+            if (direction == ListSortDirection.Ascending)
+            {
+                headerClicked.Column.HeaderTemplate =
+                    Resources["HeaderTemplateArrowUp"] as DataTemplate;
+            }
+            else
+            {
+                headerClicked.Column.HeaderTemplate =
+                    Resources["HeaderTemplateArrowDown"] as DataTemplate;
+            }
+
+            // Remove arrow from previously sorted header
+            if (_lastHeaderClicked != null && _lastHeaderClicked != headerClicked)
+            {
+                _lastHeaderClicked.Column.HeaderTemplate =
+                    Resources["HeaderTemplateDefault"] as DataTemplate;
+            }
+
+            _lastHeaderClicked = headerClicked;
+        }
+
+        public static ListSortDirection ApplySort(ICollectionView view, string propertyName)
+        {
+            ListSortDirection direction = ListSortDirection.Ascending;
+            if (view.SortDescriptions.Count > 0)
+            {
+                SortDescription currentSort = view.SortDescriptions[0];
+                if (currentSort.PropertyName == propertyName)
+                {
+                    if (currentSort.Direction == ListSortDirection.Ascending)
+                        direction = ListSortDirection.Descending;
+                    else
+                        direction = ListSortDirection.Ascending;
+                }
+                view.SortDescriptions.Clear();
+            }
+            if (!string.IsNullOrEmpty(propertyName))
+            {
+                view.SortDescriptions.Add(new SortDescription(propertyName, direction));
+            }
+            return direction;
+        }
+
     }
-    
 }
 
 
-/*
-
-
- Pool"                   
- Jacuzzi"                
- Garden"                 
- ChildrensAttractions"   
- SpredBads"              
- AirCondsner"            
- frisider"               
- SingogNaerBy"           
- NaerPublicTrensportion"        
-*/
